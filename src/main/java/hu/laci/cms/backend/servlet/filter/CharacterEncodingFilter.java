c6efd13643ce3f05168e21f5fefd37ce1ff133a8
Vizi Laszlo <vizilaszlo01@gmail.com>
2026-05-16 22:21:23 +0200

    utf8 filter

---
+++ src/main/java/hu/laci/cms/backend/servlet/filter/CharacterEncodingFilter.java
@@ -0,0 +1,21 @@
+package hu.laci.cms.backend.servlet.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class CharacterEncodingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        request.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        chain.doFilter(request, response);
+    }
+}
